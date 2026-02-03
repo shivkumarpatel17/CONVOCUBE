0e8b4df7b6ec2459f758dfa00e5e0b43d82c29bf
SHIVKUMAR PATEL <patel-s46@webmail.uwinnipeg.ca>
2026-02-02 21:19:19 -0600

    Add files via upload

--- client/src/components/styles/StyledComponents.jsx
+++ client/src/components/styles/StyledComponents.jsx
@@ -1,1 +1,18 @@
+import { Link as LinkComponent} from 'react-router-dom'
+import styled from 'styled-components';
 
+export const Link = styled(LinkComponent)`
+    text-decoration: none;
+    color: black;
+    margin: 0px 0px;
+`
+
+export const InputBox = styled("input")`
+    widht: 100%;
+    height: "100%";
+    border: none;
+    outline: none;
+    margin:  0;
+    padding: 8px 4rem;
+    border-radius: 1.5rem;
+`
