02524e474bc8c6d8e05e22465102380c8a0f4564
SHIVKUMAR PATEL <patel-s46@webmail.uwinnipeg.ca>
2026-02-02 22:01:08 -0600

    Add files via upload

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,1 +1,9 @@
+const corsOptions = {
+    origin: ['http://localhost:5173', "http://localhost:4173", process.env.CLIENT_URL, "https://convocube.online", "https://www.convocube.online",  "http://10.0.0.144:5173", "*"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}
 
+const CHATAPP_TOKEN = "chatapp-token";
+
+export { corsOptions, CHATAPP_TOKEN }
