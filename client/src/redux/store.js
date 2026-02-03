fb908d4edb60bdfe92eca9050cda0f6cd10195b9
SHIVKUMAR PATEL <patel-s46@webmail.uwinnipeg.ca>
2026-02-02 21:52:16 -0600

    Add files via upload

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,1 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./reducers/auth";
+import api from "./api/api";
+import miscSlice from "./reducers/misc";
+import chatSlice from "./reducers/chat";
 
+
+const store = configureStore({
+    reducer: {
+        [authSlice.name]: authSlice.reducer, // User authentication state
+        [miscSlice.name]: miscSlice.reducer, // UI modal and state flags
+        [chatSlice.name]: chatSlice.reducer, // Chat-related state
+        [api.reducerPath]: api.reducer, // API state managed by RTK Query
+    },
+    middleware: (getDefaultMiddleware) => [
+        ...getDefaultMiddleware(), 
+        api.middleware, // Include the API middleware for query caching, etc.
+    ],
+});
+
+export default store;
