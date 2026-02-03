086389ddd31015794491e2b4636d08756951eb6c
SHIVKUMAR PATEL <patel-s46@webmail.uwinnipeg.ca>
2026-02-02 21:12:24 -0600

    Add files via upload

--- client/src/components/auth/ProtectRoute.jsx
+++ client/src/components/auth/ProtectRoute.jsx
@@ -1,1 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
+/**
+ * ProtectRoute Component
+ * This component protects routes by ensuring a user is logged in.
+ * If the user is not logged in, it redirects them to the specified redirect path (default is "/login").
+ */
+const ProtectRoute = ({ children, user, redirect = "/login" }) => {
+    // If no user is logged in, redirect to the specified path
+    if (!user) return <Navigate to={redirect} />;
+    
+    // If children are provided, render them; otherwise, render the child route using Outlet
+    return children ? children : <Outlet />;
+};
+
+
+export default ProtectRoute
