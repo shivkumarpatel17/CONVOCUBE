6d289c8a291fc70fd9b9b413c1fbe88977408394
SHIVKUMAR PATEL <patel-s46@webmail.uwinnipeg.ca>
2026-02-02 22:10:07 -0600

    Add files via upload

--- server/seeders/user.js
+++ server/seeders/user.js
@@ -1,1 +1,34 @@
+import User from '../models/user.js';
+import {faker} from '@faker-js/faker';
 
+
+const createUser = async (numUsers) => {
+    try{
+
+        const usersPromise = [];
+
+        for(let i = 0; i < numUsers; i++){
+            const tempUser = User.create({
+                name: faker.person.fullName(),
+                username: faker.internet.userName(),
+                bio: faker.lorem.sentence(10),
+                password: "password",
+                avatar:{
+                    url: faker.image.avatar(),
+                    public_id: faker.system.fileName(),
+                }
+            });
+            usersPromise.push(tempUser);
+        }
+
+        await Promise.all(usersPromise);
+
+        console.log("User created", numUsers);
+
+    } catch(err){
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+export { createUser }
